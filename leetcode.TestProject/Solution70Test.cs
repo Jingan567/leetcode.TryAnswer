9f889cc22ed1b69bd3443130eafcc0ee7fe72361
李静安 <3081309224@qq.com>
2025-06-27 14:20:26 +0800

    添加项目文件。

---
+++ leetcode.TestProject/Solution70Test.cs
@@ -0,0 +1,28 @@
+﻿using leetcode.TryAnswer;
+
+namespace leetcode.TestProject;
+
+public class Solution70Test
+{
+    /// <summary>
+    /// 这是一个带有 [SetUp] 特性的空方法。
+    /// NUnit 会在每个测试方法执行前自动调用它，通常用于初始化测试环境或准备测试数据。
+    /// 当前方法体为空，表示没有特殊的初始化逻辑。
+    /// </summary>
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    /// <summary>
+    /// 这是一个带有 [Test] 特性的测试方法。NUnit 会自动识别并运行它。
+    /// 方法体内调用了 Assert.Pass();，这表示该测试无条件通过。
+    /// </summary>
+    [Test]
+    public void ClimbStairsTest1()
+    {
+        Solution70 solution = new Solution70();
+        var res = solution.ClimbStairs();
+        
+    }
+}
---
+++ leetcode.TryAnswer/Solution70.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode.TryAnswer
+{
+    #region 70. 爬楼梯
+    /*
+     * 假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+     * 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+     * 
+     * 示例 1：
+     * 输入：n = 2
+     * 输出：2
+     * 解释：有两种方法可以爬到楼顶。
+     * 1. 1 阶 + 1 阶
+     * 2. 2 阶
+     * 
+     * 示例 2：
+     * 输入：n = 3
+     * 输出：3
+     * 解释：有三种方法可以爬到楼顶。
+     * 1. 1 阶 + 1 阶 + 1 阶
+     * 2. 1 阶 + 2 阶
+     * 3. 2 阶 + 1 阶
+     * 
+     * 提示：
+     * 1 <= n <= 45
+     */
+    #endregion
+    public class Solution70
+    {
+        public int ClimbStairs(int n)
+        {
+            #region 错误代码
+            //这种方法是错误的，因为它会计算出所有可能的组合，但并没有考虑到重复的情况。
+            //int count = 0;
+            //for (int x = 0; x < 45; x++)
+            //{
+            //    for (int y = 0; y < 23; y++)
+            //    {
+            //        if ((x + 2 * y) == n) count++;
+            //    }
+            //}
+            //return count;
+            #endregion
+
+
+            #region 正确代码
+            //动态规划+滚动数组
+            int p = 0, q = 0, r = 1;
+            for (int i = 1; i <= n; ++i)
+            {
+                p = q;
+                q = r;
+                r = p + q;
+            }
+            return r;
+            #endregion
+        }
+    }
+}
